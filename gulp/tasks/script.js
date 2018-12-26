af054ac981867c2ceec69ff57474c303fc7336eb
unknown <PC_02607@PC_02607.main.unxp.relc.com>
2018-12-26 16:54:46 +0200

    Сверстал секцию banner

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -1,7 +1,7 @@
 module.exports = function(){
     $.gulp.task('scripts:lib', function () {
-        return $.gulp.src('src/static/js/libs/*.js')
-        // return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+        // return $.gulp.src('src/static/js/libs/*.js')
+        return $.gulp.src(['node_modules/jquery/dist/jquery.min.js','node_modules/svg4everybody/dist/svg4everybody.min.js', 'node_modules/slick-carousel/slick/slick.min.js'])
         .pipe ($.gp.concat('libs.min.js'))
         .pipe ($.gulp.dest('build/static/js/'))
         .pipe($.bs.reload({
@@ -10,7 +10,7 @@ module.exports = function(){
     });
 
     $.gulp.task('scripts', function () {
-        return $.gulp.src('src/static/js/main.js')
+				return $.gulp.src('src/static/js/main.js')
         .pipe ($.gulp.dest('build/static/js/'))
         .pipe($.bs.reload({
             stream: true
---
+++ src/static/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	svg4everybody({});
+
+	let mainSubnavHover = () => {
+		$('.main-subnav__item').hover(function() {
+			let parentList = $(this).closest('.main-subnav__list');
+			if ($(this).children('.main-subnav__list').length) {
+				let catNavHeight = $(this).children('.main-subnav__list').outerHeight();
+				if (parentList.outerHeight() < catNavHeight) {
+					parentList.css('width', '720');
+				}
+				parentList.css('width', '720');
+			}
+		}, function() {
+				let parentList = $(this).closest('.main-subnav__list');
+				parentList.css('height', 'auto');
+				parentList.css('width', 'auto');
+		})
+	};
+
+	let openSearchForm = () => {
+		$(document).on('click', '.search__icon', function() {
+			$(this).parent().addClass('search--open');
+		})
+	};
+
+	let clearSearchForm = () => {
+		$(document).on('click', '.search__clear-icon', function() {
+			$('.search__input').val('');
+		})
+	};
+
+	let bannerSlider = () => {
+		$('.js-banner').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			prevArrow: ".banner__navigation--prev",
+			nextArrow: ".banner__navigation--next",
+			dots: true,
+			customPaging: function(slider, i) {
+				return '<a class="banner__dot"></a>'
+			}
+		});
+	};
+
+	mainSubnavHover();
+	openSearchForm();
+	clearSearchForm();
+	bannerSlider();
+});
