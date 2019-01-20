d26fb15e432e82c16b92bfa4b7c2ad18a047bf23
Zheronimo <kirichenko.e.o@gmail.com>
2019-01-20 22:25:48 +0200

    Начал адаптив

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -84,6 +84,34 @@ $(document).ready(function() {
 		$('.js-products-line-slider').slick({
 			slidesToShow: 4,
 			slidesToScroll: 1,
+			responsive: [
+		    {
+		      breakpoint: 1139,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
 		});
 	};
 
