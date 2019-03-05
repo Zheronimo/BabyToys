f538a0394498cc0c0e16bf23271070b8dea1dba1
Zheronimo <kirichenko.e.o@gmail.com>
2019-03-06 00:29:39 +0200

    Добавлены изменения в goods, product, details

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -264,7 +264,39 @@ $(document).ready(function() {
 		$(document).on('click', '.color-list__item', function () {
 			$(this).addClass('color-list__item--active').siblings().removeClass('color-list__item--active');
 		});
-	}; 
+	};
+	
+	let scrollToElement = () => {
+		$('.scroll-link')
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function (event) {
+				if (
+						location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+						&&
+						location.hostname == this.hostname
+				) {
+						var target = $(this.hash);
+						target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+						if (target.length) {
+								event.preventDefault();
+								$('html, body').animate({
+										scrollTop: target.offset().top - 50
+								}, 1000, function () {
+										var $target = $(target);
+										$target.focus();
+										if ($target.is(":focus")) {
+												return false;
+										} else {
+												$target.attr('tabindex', '-1');
+												$target.focus();
+										}
+										;
+								});
+						}
+				}
+		});
+	};
 
 	mainSubnavHover();
 	openSearchForm();
@@ -282,6 +314,7 @@ $(document).ready(function() {
 	filterToggle();
 	productSlider();
 	colorClicked();
+	scrollToElement();
 });
 
 $(window).on('resize',function () {
